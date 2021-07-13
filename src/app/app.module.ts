2277c828278e87fc9cd2a323be7869a0ac1923f2
devMonkey87 <jlandetemunoz@icloud.com>
2021-07-13 22:35:14 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NxButtonModule } from '@aposin/ng-aquila/button';
+import { NxCheckboxModule } from '@aposin/ng-aquila/checkbox';
+import { NxDocumentationIconModule } from '@aposin/ng-aquila/documentation-icons';
+import { NxDropdownModule } from '@aposin/ng-aquila/dropdown';
+import { NxFooterModule } from '@aposin/ng-aquila/footer';
+import { NxFormfieldModule } from '@aposin/ng-aquila/formfield';
+import { NxGridModule } from '@aposin/ng-aquila/grid';
+import { NxHeadlineModule } from '@aposin/ng-aquila/headline';
+import { NxIconModule } from '@aposin/ng-aquila/icon';
+import { NxInputModule } from '@aposin/ng-aquila/input';
+import { NxLinkModule } from '@aposin/ng-aquila/link';
+import { NxMessageModule } from '@aposin/ng-aquila/message';
+import { NxModalModule } from '@aposin/ng-aquila/modal';
+import { NxOverlayModule } from '@aposin/ng-aquila/overlay';
+import { NxPopoverModule } from '@aposin/ng-aquila/popover';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +29,30 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientJsonpModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([]),
+    NxButtonModule,
+    NxCheckboxModule,
+    NxDocumentationIconModule,
+    NxDropdownModule,
+    NxFooterModule,
+    NxFormfieldModule,
+    NxGridModule,
+    NxHeadlineModule,
+    NxIconModule,
+    NxInputModule,
+    NxLinkModule,
+    NxMessageModule,
+    NxModalModule,
+    NxOverlayModule,
+    NxPopoverModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+/**  Copyright APOSIN 2021 */
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,3 +63,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import cssVars from 'css-vars-ponyfill';
+cssVars({ watch: true, onlyLegacy: true, shadowDOM: true});
